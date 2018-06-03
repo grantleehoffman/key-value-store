4e3e59c861cb041a564fbce7f39f115bb0ad6cb5
grantleehoffman <grantleehoffman@gmail.com>
2018-06-03 11:13:19 -0700

    improve readability in deploy script

--- scripts/setup_and_run_deployment_pipeline.sh
+++ scripts/setup_and_run_deployment_pipeline.sh
@@ -40,22 +40,32 @@ if [[ -z ${source_bucket} || -z ${profile} || -z ${region} || -z ${pipeline_acti
 fi
 
 if [[ ${cfn_parameters} ]];then
+  printf "==== Build CFN Parameters ====\n"
   scripts/build_parameter_files.sh
+  printf "==============================\n\n\n"
 fi
 
 set +e
 stack_status=$(aws cloudformation describe-stacks --stack-name kv-pipeline --profile "${profile}" --region "${region}" --query Stacks[].StackStatus --output text 2> /dev/null)
 set -e
 
+printf "========== Build CLI ==========\n"
 ./build.sh
+printf "===============================\n\n\n"
+
+echo "=== Upload Deployment Artifact =="
 aws s3 cp deployment-artifact.zip "s3://${source_bucket}/deployment-artifact.zip" --profile "${profile}"
+printf "===============================\n\n\n"
 
 if [[ ${stack_status} != "CREATE_COMPLETE" ]] && [[ ${stack_status} != "UPDATE_COMPLETE" ]];then
+printf "=== Pipeline Stack not found ==\n"
+printf "=== Creating pipeline stack ===\n"
   aws cloudformation create-stack --stack-name kv-pipeline --template-body file://cfn/pipeline.json \
      --capabilities CAPABILITY_IAM --region "${region}" --parameters \
      "[{\"ParameterKey\":\"SourceArtifactBucket\",\"ParameterValue\":\"${source_bucket}\"},\
      {\"ParameterKey\":\"SourceArtifactKey\",\"ParameterValue\":\"deployment-artifact.zip\"},\
      {\"ParameterKey\":\"PipelineBucket\",\"ParameterValue\":\"${pipeline_action_bucket}\"}]"
-  echo "stack creation initiated"
+printf "===============================\n\n\n"
 fi
-echo "pipeline started"
+echo "Tasks complete, new pipeline revision queued"
+
