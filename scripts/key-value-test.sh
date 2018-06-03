b092c0c50ceb1e03327b856ede0cacf8d68f7023
grantleehoffman <grantleehoffman@gmail.com>
2018-06-02 22:18:04 -0700

    cleaner test messages

--- scripts/key-value-test.sh
+++ scripts/key-value-test.sh
@@ -3,37 +3,37 @@
 script_exit=0
 ./key-value put --key test --value testvalue -s ${server_url}
 if [ $? != 0 ];then
-  echo '=== ./key-value put --key test --value testvalue -s ${server_url} exit code is not 0 x ==='
+  echo '=== ./key-value put --key test --value testvalue exit code is not 0 x ==='
   script_exit=1
 else
-  echo '=== ./key-value put --key test --value testvalue -s ${server_url} exit 0 ✓ ==='
+  echo '=== ./key-value put --key test --value testvalue exit code 0 ✓ ==='
 fi
 echo ""
 
 output=$(./key-value get --key test -s ${server_url})
 if [ $? != 0 ] || [ ${output} != "testvalue" ]; then
-  echo '=== ./key-value get --key test -s ${server_url} output is not "testvalue" x ==='
+  echo '=== ./key-value get --key test output is not "testvalue" x ==='
   script_exit=1
 else
-  echo '=== ./key-value get --key test -s ${server_url} output is "testvalue" ✓ ==='
+  echo '=== ./key-value get --key test output is "testvalue" ✓ ==='
 fi
 echo ""
 
 ./key-value delete --key test -s ${server_url} &> /dev/null
 if [ $? != 0 ];then
-  echo '=== ./key-value delete --key test -s ${server_url} exit is not 0 x ==='
+  echo '=== ./key-value delete --key test exit code is not 0 x ==='
   script_exit=1
 else
-  echo '=== ./key-value delete --key test -s ${server_url} exit 0 ✓ ==='
+  echo '=== ./key-value delete --key test exit code 0 ✓ ==='
 fi
 echo ""
 
 ./key-value get --key test -s ${server_url} &> /dev/null
 if [ $? != 1 ];then
-  echo '=== ./key-value get --key test -s ${server_url} exit is not 1 x ==='
+  echo '=== ./key-value get --key test exit code is not 1 x ==='
   script_exit=1
 else
-  echo '=== ./key-value get --key test -s ${server_url} exit 1 ✓ ==='
+  echo '=== ./key-value get --key test exit code 1 ✓ ==='
 fi
 echo ""
 
